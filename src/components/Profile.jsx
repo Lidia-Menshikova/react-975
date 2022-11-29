819627c023444dd7ff78409987cba51972a809b3
Lidia-Menshikova <xwp2007@mail.ru>
2022-11-29 09:28:18 +0300

    add Profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+  return (
+    <div className="row">
+      <div className="col-sm-3">
+        <img
+          src="https://www.seekpng.com/png/full/356-3562377_personal-user.png"
+          alt=""
+          width={"70%"}
+        />
+      </div>
+      <div className="col-sm-9">
+        <h1>Фамилия и имя</h1>
+        <h2>Рассказ о себе</h2>
+        <p>E-mail: ivan@ya.ru</p>
+        <p>Id: 2</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Voluptatibus, ipsa impedit. A vitae officiis quam dicta reprehenderit
+          cupiditate molestias debitis sint ab mollitia, vel fuga tempore fugit
+          veritatis eos praesentium!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
