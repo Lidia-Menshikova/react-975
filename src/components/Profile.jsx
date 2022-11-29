44b988a7c0fc85fa963d840d00d9e832ad99ea42
Lidia-Menshikova <xwp2007@mail.ru>
2022-11-29 10:46:23 +0300

    bag fixed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,26 @@
 import styles from "./Profile.module.css";
 
 const Profile = (props) => {
-  let user = props.functiion();
-  console.log(user);
+  let user = props.function();
+  //console.log(user);
   return (
     <div className="row">
       <div className="col-sm-3">
-        <img
-          src="https://www.seekpng.com/png/full/356-3562377_personal-user.png"
-          alt=""
-          width={"70%"}
-        />
+        <img src={user.avatar} alt="" width={"100%"} />
       </div>
       <div className="col-sm-9">
-        <h1 style={{ backgroundColor: "lightblue" }}>{user.name} </h1>
-        <h2 className={styles.about}>Рассказ о себе</h2>
-        {user.lastname}
+        <h1 style={{ backgroundColor: "lightblue" }}>
+          {user.name} {user.lastname}
+        </h1>
+        <h2 className={styles.about}>{user.about}</h2>
         <p>E-mail: {user.email}</p>
         <p>Id: {user.id}</p>
-        <p className={styles.paragraph}>{user.about}</p>
+        <p className={styles.paragraph}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magnam
+          nesciunt earum similique mollitia nam numquam consequatur
+          necessitatibus facere eaque aliquid, sed laborum accusantium corrupti
+          enim debitis? Fugit, fugiat voluptatum!
+        </p>
       </div>
     </div>
   );
