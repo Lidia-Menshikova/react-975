ed4a7725cf0bfcbbcf64fe863f6d16f41a578f41
Lidia-Menshikova <xwp2007@mail.ru>
2022-12-01 10:40:39 +0300

    add Tailwind, Shop

---
+++ src/components/Shop.jsx
@@ -0,0 +1,82 @@
+const products = [
+  {
+    id: 1,
+    name: "Черная майка",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "350 руб.",
+    color: "Черный",
+  },
+  {
+    id: 2,
+    name: "Супер ручка",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "4500 руб.",
+    color: "Бежевый",
+  },
+  {
+    id: 3,
+    name: "Серая майка",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "350 руб.",
+    color: "Серый",
+  },
+  {
+    id: 4,
+    name: "Интересная майка",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "750 руб.",
+    color: "Бежевый",
+  },
+];
+
+export default function Shop() {
+  return (
+    <div className="bg-white">
+      <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-4xl font-bold tracking-tight text-gray-900">
+          Лучшие предложения:
+        </h2>
+
+        <div className="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="relative group">
+              <div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className="object-cover object-center w-full h-full lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="flex justify-between mt-4">
+                <div>
+                  <h3 className="text-sm text-red-700">
+                    <a href={product.href}>
+                      <span aria-hidden="true" className="inset-0 ab solute" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  {product.price}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
